8dad0884ba05198a70cfd8e802be72055444e206
jimtxowebdeb <jimtxowebdeb@github.com>
2014-09-17 09:44:41 +0200

    lehenengo ariketa

---
+++ ariketa.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+</head>
+<body>
+<table border=1>
+<tr>
+
+<?php 
+
+for($i=1;$i<=100;$i++){
+?>
+	
+		<td>
+			<?php if ($i%2==0) {
+			echo "<font color= 'red'>";
+		} else {
+			echo "<font color= 'blue'>";
+		}
+		?>
+
+		<?php echo $i; 
+		if($i%10==0){
+		?>
+		</font>
+		</td>
+	</tr>
+	<tr>
+
+		<?php
+			}
+		else{
+			?>
+		</td>
+		
+		<?php
+		}
+}?>
+</tr>
+
+</table>
+
+</body>
+</html>
